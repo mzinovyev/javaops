97bbf965e84b3cc65cb59578c3ab00e88ca95eb3
zinovev.mn <m.zinovyev@gmail.com>
2020-10-21 22:13:38 +0300

    fix spaces, comments, abbreviation

--- lesson_01/Variable.java
+++ lesson_01/Variable.java
@@ -1,21 +1,29 @@
-public class Variable{
-	public static void main(String[] args){
-		boolean isIntelProcessor = true; 		//1 byte (1  bit)
-		byte totalMemoryGbytes = 8; 			//2 (1 byte)
-		short rootFoldersOnDisk = 32; 			//3 (2 byte)
-		int numberOfCores = 4; 					//4 (4 byte)
-		long totalDiskSpaceBytes = 25000000000L;//5 (8 byte)
-		float cpuFrequencyGhz = 2.7F;			//6 (4 byte)
-		double cpuMaximumFrequencyGhz = 3.48D; 	//7 (8 byte)
-		char processorGeneration = '5'; 		//8 (2 byte)
+public class Variable {
+	public static void main(String[] args) {
+		// 1  bit
+		boolean isIntelProcessor = true;
+		// 1 byte
+		byte totalMemoryGB = 8;
+		// 2 byte
+		short rootFoldersOnDisk = 32; 			
+		// 4 byte
+		int numberOfCores = 4;
+		// 8 byte
+		long totalDiskSpaceBt = 25000000000L;
+		// 4 byte
+		float cpuFrequencyGhz = 2.7F;
+		// 8 byte
+		double cpuMaximumFrequencyGhz = 3.48D; 	
+		// 2 byte
+		char processorGeneration = '5'; 		
 
-		System.out.print("isIntelProcessor: "); System.out.println(isIntelProcessor);
-		System.out.print("totalMemoryGbytes: "); System.out.println(totalMemoryGbytes);
-		System.out.print("rootFoldersOnDisk: "); System.out.println(rootFoldersOnDisk);
-		System.out.print("numberOfCores: "); System.out.println(numberOfCores);
-		System.out.print("totalDiskSpaceBytes: "); System.out.println(totalDiskSpaceBytes);
-		System.out.print("cpuFrequencyGhz: "); System.out.println(cpuFrequencyGhz);
-		System.out.print("cpuMaximumFrequencyGhz: "); System.out.println(cpuMaximumFrequencyGhz);
-		System.out.print("processorGeneration: "); System.out.println(processorGeneration);
+		System.out.println(isIntelProcessor);
+		System.out.println(totalMemoryGB);
+		System.out.println(rootFoldersOnDisk);
+		System.out.println(numberOfCores);
+		System.out.println(totalDiskSpaceBt);
+		System.out.println(cpuFrequencyGhz);
+		System.out.println(cpuMaximumFrequencyGhz);
+		System.out.println(processorGeneration);
 	}
 }
