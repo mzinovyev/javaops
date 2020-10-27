6c43e37ec4bc2d7bbe6b2db4e6f7fa3facf63d47
zinovev.mn <m.zinovyev@gmail.com>
2020-10-27 14:22:07 +0300

    fix variables

--- lesson_01/ConditionalStatement.java
+++ lesson_01/ConditionalStatement.java
@@ -1,31 +1,31 @@
 public class ConditionalStatement {
 	public static void main (String[] args) {
-		byte personAge = 35;
-		char personSex = 'W';
-		float personHeight = 1.85F;
-		char personNameFirstLetter = 'M';
 
-		if (personAge > 20) {
+		byte age = 35;
+		if (age > 20) {
 			System.out.println("Person age is above 20 years");
 		}
-		
-		if (personSex == 'M') {
+
+		char sex = 'W';		
+		if (sex == 'M') {
 			System.out.println("Person sex is Man");
 		}
 
-		if (personSex != 'M') {
+		if (sex != 'M') {
 			System.out.println("Person sex is Wooman");
 		}
 
-		if (personHeight < 1.80) {
+		float height = 1.85F;
+		if (height < 1.80) {
 			System.out.println("Person height is less then 180 cm");
 		} else {
 			System.out.println("Person height is equal or above then 180 cm");
 		}
 
-		if (personNameFirstLetter == 'M') {
+		char firstLetter = 'M';
+		if (firstLetter == 'M') {
 			System.out.println("Person name first letter is 'M'");
-		} else if (personNameFirstLetter == 'I') {
+		} else if (firstLetter == 'I') {
 			System.out.println("Person name first letter is 'I'");
 		} else {
 			System.out.println("Person name first letter is neither 'M' nor 'I' ");
