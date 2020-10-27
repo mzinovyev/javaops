aae4a6c35dd0c119d042efb3cf49d0c3242c0eb3
zinovev.mn <m.zinovyev@gmail.com>
2020-10-27 13:58:22 +0300

    use abbreviation

--- lesson_01/Variable.java
+++ lesson_01/Variable.java
@@ -1,29 +1,29 @@
 public class Variable {
 	public static void main(String[] args) {
 		// 1  bit
-		boolean isIntelProcessor = true;
+		boolean cpu = true;
 		// 1 byte
-		byte totalMemoryGB = 8;
+		byte ram = 8;
 		// 2 byte
 		short rootFoldersOnDisk = 32; 			
 		// 4 byte
-		int numberOfCores = 4;
+		int cpuCores = 4;
 		// 8 byte
-		long totalDiskSpaceBt = 25000000000L;
+		long hddCapacityBt = 25000000000L;
 		// 4 byte
-		float cpuFrequencyGhz = 2.7F;
+		float cpuGhz = 2.7F;
 		// 8 byte
-		double cpuMaximumFrequencyGhz = 3.48D; 	
+		double cpuMaxGhz = 3.48D; 	
 		// 2 byte
-		char processorGeneration = '5'; 		
+		char cpuGeneration = '5'; 		
 
-		System.out.println(isIntelProcessor);
-		System.out.println(totalMemoryGB);
+		System.out.println(cpu);
+		System.out.println(ram);
 		System.out.println(rootFoldersOnDisk);
-		System.out.println(numberOfCores);
-		System.out.println(totalDiskSpaceBt);
-		System.out.println(cpuFrequencyGhz);
-		System.out.println(cpuMaximumFrequencyGhz);
-		System.out.println(processorGeneration);
+		System.out.println(cpuCores);
+		System.out.println(hddCapacityBt);
+		System.out.println(cpuGhz);
+		System.out.println(cpuMaxGhz);
+		System.out.println(cpuGeneration);
 	}
 }
