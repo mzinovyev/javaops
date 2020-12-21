f041da14e3165c4229a99d9d9c3ed8298a2c99c3
mzinovyev <m.zinovyev@gmail.com>
2020-12-21 20:03:00 +0300

    убрал пробел после имени меирда и добаивл межу ){

--- lesson_01/PrintSymbols.java
+++ lesson_01/PrintSymbols.java
@@ -1,6 +1,6 @@
 public class PrintSymbols {
-	public static void main (String[] args) {
-		for (int i = 33; i < 127; i++){
+	public static void main(String[] args) {
+		for (int i = 33; i < 127; i++) {
 		 	System.out.println((char) i);
 		 }
 	}
