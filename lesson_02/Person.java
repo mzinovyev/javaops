7578307be97973d26f34cae0714d4a7e01d34d91
mzinovyev <m.zinovyev@gmail.com>
2021-01-04 11:20:08 +0300

    first commit

---
+++ lesson_02/Person.java
@@ -0,0 +1,28 @@
+class Person {
+	
+	char sex;
+	String name;
+	short height;
+	short weight;
+	short age;
+
+	public void walk() {
+
+	}
+
+	public void sit() {
+
+	}
+
+	public void run() {
+
+	}
+
+	public void think() {
+
+	}
+
+	public void learn() {
+
+	}
+}
