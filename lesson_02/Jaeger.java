b19d76986f4e8ad641bef893e8aec4cd828194e4
mzinovyev <m.zinovyev@gmail.com>
2021-01-08 14:45:18 +0300

    add getters\setters

--- lesson_02/Jaeger.java
+++ lesson_02/Jaeger.java
@@ -1,46 +1,87 @@
 public class Jaeger {
-	String modelName;
-	String mark;
-	String origin;
-	float height;
-	float weight;
-	int speed;
-	int strength;
-	int armor;
-	String pilots;
-	String weapon1;
-	String weapon2;
+	private String modelName;
+	private String mark;
+	private String origin;
+	private float height;
+	private float weight;
+	private int speed;
+	private int strength;
+	private int armor;
+	private String pilots;
+	private String weapon1;
+	private String weapon2;
 
-	void setTechInfo(String modelName, String mark, String origin) {
+	public void setModelName(String modelName) {
 		this.modelName = modelName;
+	}
+	public void setMark(String mark) {
 		this.mark = mark;
+	}
+	public void setOrigin(String origin) {
 		this.origin = origin;
 	}
-
-	void setSpecifications(float height, float weight, int speed, int strength, int armor) {
+	public void setHeight(float height) {
 		this.height = height;
+	}
+	public void setWeight(float weight) {
 		this.weight = weight;
+	}
+	public void setSpeed(int speed) {
 		this.speed = speed;
+	}
+	public void setStrength(int strength) {
 		this.strength = strength;
-		this.armor = armor;		
 	}
-
-	void setWeapons(String weapon1, String weapon2) {
-		this.weapon1 = weapon1;
-		this.weapon2 = weapon2;		
+	public void setArmor(int armor) {
+		this.armor = armor;	
 	}
-
-	void setPilots(String pilots) {
+	public void setPilots(String pilots) {
 		this.pilots = pilots;
 	}
-
-	String getName() {
-		return modelName;
+	public void setWeapon1(String weapon1) {
+		this.weapon1 = weapon1;
 	}
+	public void setWeapon2(String weapon2) {
+		this.weapon2 = weapon2;	
+	}
+
+	public String getModelName() {
+		return modelName; 	
+	}
+	public String getMark() {
+		return mark;
+	}
+	public String getOrigin() {
+		return origin;
+	}
+	public float setHeight() {
+		return height;
+	}
+	public float setWeight() {
+		return weight;
+	}
+	public int setSpeed() {
+		return speed;
+	}
+	public int getStrength() {
+		return strength;
+	}
+	public int getArmor() {
+		return armor;	
+	}
+	public String getPilots() {
+		return pilots;
+	}
+	public String getWeapon1() {
+		return weapon1;
+	}
+	public String getWeapon2() {
+		return weapon2;	
+	}
+
 
 	void drift() {
 		System.out.println("Drift with pilots " + pilots);
-
 	}
 
 	void move() {
--- lesson_02/JaegerTest.java
+++ lesson_02/JaegerTest.java
@@ -1,7 +1,7 @@
 public class JaegerTest {
 	protected static void testJaeger(Jaeger j) {
 		System.out.println("---Start testing jaeger---");
-		System.out.println("lounch jaeger " + j.getName());
+		System.out.println("lounch jaeger " + j.getModelName());
 		j.drift();
 		j.move();
 		j.useWeapon();		
@@ -9,15 +9,29 @@ public class JaegerTest {
 
 	public static void main(String[] args) {
 		Jaeger j1 = new Jaeger();
-		j1.setTechInfo("Striker Eureka", "Mark-5", "Australia");
-		j1.setSpecifications(76.2F, 1.850F, 10, 10, 9);
-		j1.setWeapons("Assault Mount", "WMB2x90");
+		j1.setModelName("Striker Eureka");
+		j1.setMark("Mark-5");
+		j1.setOrigin("Australia");
+		j1.setHeight(76.2F);
+		j1.setWeight(1.850F);
+		j1.setSpeed(10);
+		j1.setStrength(10);
+		j1.setArmor(9);
+		j1.setWeapon1("Assault Mount");
+		j1.setWeapon2("WMB2x90");
 		j1.setPilots("Herc Hansen, Stacker");
 
 		Jaeger j2 = new Jaeger();
-		j2.setTechInfo("Crimson Typhoon", "Mark-4", "China");
-		j2.setSpecifications(76.2F, 1.722F, 9, 8, 6);
-		j2.setWeapons("IB22 Plasmacaster", "empty");
+		j2.setModelName("Crimson Typhoon");
+		j2.setMark("Mark-4");
+		j2.setOrigin("China");
+		j2.setHeight(76.2F);
+		j2.setWeight(1.722F);
+		j2.setSpeed(9);
+		j2.setStrength(9);
+		j2.setArmor(6);
+		j2.setWeapon1("IB22 Plasmacaster");
+		j2.setWeapon2("none");
 		j2.setPilots("Cheung Wei, Jin Wei");
 
 		testJaeger(j1);
