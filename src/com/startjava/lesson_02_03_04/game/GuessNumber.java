1e5f322ed27cf9be65270fba54518f76845c8dab
mzinovyev <m.zinovyev@gmail.com>
2021-01-18 21:10:10 +0300

    refactor game 2 use array

--- src/com/startjava/lesson_02_03_04/game/GuessNumber.java
+++ src/com/startjava/lesson_02_03_04/game/GuessNumber.java
@@ -1,5 +1,8 @@
 package com.startjava.lesson_02_03_04.game;
 
+import com.sun.javaws.IconUtil;
+
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -16,18 +19,19 @@ public class GuessNumber {
 	public void start() {
 		Random rnd = new Random(System.currentTimeMillis());
 		secretNum = rnd.nextInt(100) + 1;
-		String winner = "";
-		do {
+		// Для отладки выводит секретное число
+		System.out.println("Загаданное число: " + secretNum);
+		System.out.println("У Вас 10 попыток чтобы угадать число");
+		for (int i = 0; i < 3; i++) {
 			if (makeMoove(player1)) {
-				winner = player1.getName();
 				break;
 			}
 			if (makeMoove(player2)) {
-				winner = player2.getName();
 				break;
 			}
-		} while (true);
-		System.out.println("Победил игрок " + winner);
+		}
+		System.out.println(Arrays.toString(player1.getAttempts()));
+		System.out.println(Arrays.toString(player2.getAttempts()));
 	}
 
 	private boolean checkNumber(int num) {
@@ -40,19 +44,26 @@ public class GuessNumber {
 		}
 		return false;
 	}
-
 	// Игорок делает свой ход в игре, если угадал число - возвращаем true
 	private boolean makeMoove(Player player) {
 		inputNumber(player);
-		if (checkNumber(player.getNumber())) {
+		if (checkNumber(player.getLastNumber())) {
+			System.out.println("Игрок " + player.getName() + " угадал число " + player.getLastNumber() + " c " + player.getAttemptCount() + " попытки");
 			return true;
+		}
+		if (player.getAttemptCount() == 3) {
+			System.out.println("У " + player.getName() + " закончились попытки");
 		}
 		return false;
 	}
-
+	// Запрашивает число у игрока пока есть попытки
 	private void inputNumber(Player player) {
-		Scanner scan = new Scanner(System.in);
-		System.out.print(player.getName() + " введите загаданное число: ");
-		player.setNumber(scan.nextInt());		
+		if (player.getAttemptCount() <= 3) {
+			Scanner scan = new Scanner(System.in);
+			System.out.print(player.getName() + " введите загаданное число: ");
+			player.addNumber(scan.nextInt());
+		} else {
+			System.out.println("У " + player.getName() + " закончились попытки");
+		}
 	}
 }
--- src/com/startjava/lesson_02_03_04/game/Player.java
+++ src/com/startjava/lesson_02_03_04/game/Player.java
@@ -1,22 +1,35 @@
 package com.startjava.lesson_02_03_04.game;
 
+import java.util.Arrays;
+
 public class Player {
 	private String name;
-	private int number;
+	private int[] numbers;
+	private int nextIndex; //индекс последнего введенного числа
 
 	public Player(String name) {
 		this.name = name;
+		numbers = new int[3];
+		nextIndex = 0;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public void setNumber(int number) {
-		this.number = number;
+	public void addNumber(int number) {
+		numbers[nextIndex++] = number;
 	}
 	
-	public int getNumber() {
-		return number;
+	public int getLastNumber() {
+		return numbers[nextIndex-1];
+	}
+	// Возвращает кол-во чисел загаданных игороком
+	public int getAttemptCount() {
+		return nextIndex;
+	}
+	// Возвращает список чисел
+	public int[] getAttempts() {
+		return Arrays.copyOf(numbers, nextIndex);
 	}
 }
